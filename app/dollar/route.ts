a6a380b1507fc7e83873b2f6cbb04f3bdc4e150c
Yixuan Xu <yixuanxu94@outlook.com>
2024-11-16 12:49:49 +0800

    feat: automaticlly find dollar price at next page

--- app/dollar/route.ts
+++ app/dollar/route.ts
@@ -9,34 +9,39 @@ export const GET = async () => {
   })
 }
 
-const getBocExchangeRatePage0 = async () => {
-  const html = await fetch("https://www.boc.cn/SOURCEDB/WHPJ/index.html", {
-    cache: 'no-store'
-  }).then(v => v.text())
-  const $ = cheerio.load(html)
-  const table = $('div.BOC_main').find('table').find('tbody').find('tr').filter((i, e) => {
-    return $(e).find('td').eq(0).text().trim() === '美元'
-  }).text().split('\n').map(item => item.trim()).filter(Boolean)
-  return table.filter(Boolean)
-}
+const TRY_PAGE = 3
 
-const getBocExchangeRatePage1 = async () => {
-  const html = await fetch("https://www.boc.cn/SOURCEDB/WHPJ/index_1.html", {
-    cache: 'no-store'
-  }).then(v => v.text())
-  const $ = cheerio.load(html)
-  const table = $('div.BOC_main').find('table').find('tbody').find('tr').filter((i, e) => {
-    return $(e).find('td').eq(0).text().trim() === '美元'
-  }).text().split('\n').map(item => item.trim()).filter(Boolean)
-  return table.filter(Boolean)
+const getBocExchangeRatePage = async (currentPage = 0) => {
+  if (currentPage === 0) {
+    const html = await fetch("https://www.boc.cn/SOURCEDB/WHPJ/index.html", {
+      cache: 'no-store'
+    }).then(v => v.text())
+    const $ = cheerio.load(html)
+    const table = $('div.BOC_main').find('table').find('tbody').find('tr').filter((i, e) => {
+      return $(e).find('td').eq(0).text().trim() === '美元'
+    }).text().split('\n').map(item => item.trim()).filter(Boolean)
+    const result = table.filter(Boolean)
+    if (result.length === 0) return getBocExchangeRatePage(currentPage + 1)
+    return result
+  }
+  if(currentPage >= 1 && currentPage <= TRY_PAGE) {
+    const html = await fetch(`https://www.boc.cn/SOURCEDB/WHPJ/index_${currentPage}.html`, {
+      cache: 'no-store'
+    }).then(v => v.text())
+    const $ = cheerio.load(html)
+    const table = $('div.BOC_main').find('table').find('tbody').find('tr').filter((i, e) => {
+      return $(e).find('td').eq(0).text().trim() === '美元'
+    }).text().split('\n').map(item => item.trim()).filter(Boolean)
+    const result = table.filter(Boolean)
+    if (result.length === 0) return getBocExchangeRatePage(currentPage + 1)
+    return result
+  }
+  return []
 }
-
 
 const getBocExchangeRate = async () => {
-  const [table1, table2] = await Promise.all([getBocExchangeRatePage0(), getBocExchangeRatePage1()])
-  const table = table1.length > 0 ? 
-    table1 : table2.length > 0 ? table2 : null
-  if(!table) return `未爬取到美元数据`
+  const table = await getBocExchangeRatePage(0)
+  if (!table) return `未爬取到美元数据`
   return `中国银行${table[0]}汇率\n现汇买入价: ${table[1]}\n现汇卖出价: ${table[3]}\n发布时间: ${table[6]}\n`
 }
 
